9535941728bcaa78070d90abe07ba7102b4c04ca
Om Singh <iamomsingh11@gmail.com>
2022-08-06 23:13:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from './component/navbar';
 import './App.css';
+import Counters from './component/counters';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = { 
+    counters : [
+        {id:1, value:4},
+        {id:2, value:0},
+        {id:3, value:0},
+        {id:4, value:0},
+        {id:5, value:0},
+        {id:6, value:10},
+    ]
+ } ;
+
+ handleDelete = counterId => {
+   const counters = this.state.counters.filter( c => c.id !== counterId);
+   this.setState({counters});
+
+ };
+
+ handleReset = () => {
+    const counters = this.state.counters.map(c => {
+         c.value = 0;
+         return c;
+     });
+     this.setState({counters});
+ };
+
+ handleIncrement = (counter) => {
+     const counters = [...this.state.counters];
+     const index = counters.indexOf(counter);
+     counters[index] = {...counter};
+     counters[index].value++;
+     this.setState({counters});
+ };
+
+  render() { 
+    return (
+      <div className="App">
+        <Navbar totalCounters={this.state.counters.filter(c => c.value > 0).length}></Navbar>
+        <main className='container'>
+          <Counters
+            counters = {this.state.counters}
+            onReset = {this.handleReset}
+            onIncrement = {this.handleIncrement} 
+            onDelete = {this.handleDelete} 
+          />
+        </main> 
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
